dd21c5f4805a1cc07b366fea596bc9aab00d8ba6
Robotex <robotex@altervista.org>
2015-03-14 06:43:53 +0100

    Rewrite

--- www/js/play.js
+++ www/js/play.js
@@ -1,6 +1,6 @@
 var map, panorama, gpsTracker;
 var dirService;
-var playerMarker = null;
+var playerMarker;
 var zombies = [];
 var timeSpent = 0;
 var totalMoves = 0;
@@ -22,6 +22,8 @@ var playerData;
 var isPaused = false;
 var healthPrice = 25;
 var powerPrice = 50;
+var positionTracked = false;
+var totalZombies = 15;
 
 var app = {
     // Application Constructor
@@ -44,7 +46,42 @@ var app = {
     },
 };
 
-function Zombie() {
+function Zombie(pos) {
+    
+    var health; // Zombie's health
+
+    var route;
+    var moveInt;
+    var imNew = true;
+    var imgW = 139;
+    var imgH = 373;
+    var smallMult = 0.5; //0.05;
+    var anchorMult = 0.4;
+
+    var zombieSprites = [
+        {
+            file:'img/zombie/zombie4.png',
+            width:244,
+            height:708,
+            speed: 1,
+            anchorMult:0.3
+        },
+        {
+            file:'img/zombie/zombie5.png',
+            width:437,
+            height:664,
+            speed: 1,
+            anchorMult:0.4
+        },
+        {
+            file:'img/zombie/zombie6.png',
+            width:413,
+            height:420,
+            speed: 1,
+            anchorMult:0.6
+        }
+    ];
+
     var mapMarker = new google.maps.Marker({
         map: map,
         icon: 'img/marker/zombie.png'
@@ -55,14 +92,174 @@ function Zombie() {
         //icon: image,
         clickable: true
     });
+    var health = 100;
+    
+    function setVisible(flag) {
+        mapMarker.setVisible(flag);
+        panoramaMarker.setVisible(flag && game.isPanoramaView() ? flag : !flag);
+    }
+    
+    function isDead() {
+        return ((health <= 0) ? true : false);
+    }
+    
+    function spawn() {
+        imNew = true;
+        health = 100;
+        var center = playerMarker.getPosition();
+        var heading = Math.random()*360;
+        var dist = playerBuffer + (Math.random()*(zombieDistributionRange-playerBuffer));
+        var pos = google.maps.geometry.spherical.computeOffset(center,dist,heading);
+        mapMarker.setPosition(pos);
+        panoramaMarker.setPosition(pos);
+        setVisible(true);
+    }
+    
+    function setPosition(pos) {
+        mapMarker.setPosition(pos);
+        panoramaMarker.setPosition(pos);
+        var dist = google.maps.geometry.spherical.computeDistanceBetween(getPosition(),playerMarker.getPosition());
+
+        if (dist<dieRadius) {
+            playerData['health'] = playerData['health'] - 25;
+            updateHealthImage();
+            if (playerData['health'] <= 0) {
+                stopMove();
+                //gameOver(getPosition());
+            } else {
+                spawn();
+            }
+        }
+        // Uno zombie è dentro la visuale
+        if(dist<=zombieVisibleRadius){
+            if (zombiesInVisibleRadius == false) {
+                panorama.setVisible(true);
+                playerMarker.circle.setOptions({
+                    strokeColor: 'red',
+                    fillColor: 'red'
+                });
+                game.toggleView();
+            }
+            zombiesInVisibleRadius = true;
+            panoramaMarker.setVisible(true);
+        }
+    }
+    
+    function getPosition() {
+        return mapMarker.getPosition();
+    }
+
+     // Get directions to player
+    function getDirections(){
+        var dirReq = {
+            origin:mapMarker.getPosition(),
+            destination: playerMarker.getPosition(),
+            travelMode: google.maps.DirectionsTravelMode.WALKING
+        };
+        dirService.route(dirReq,function(result,status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                route = result.routes[0].overview_path;
+                if (imNew) {
+                    setPosition(route.shift());
+                    imNew=false;
+                } else {
+                    route[0]=getPosition();
+                }
+            }
+        });
+    }
+    
+    function redirect() {
+        getDirections();
+    }
+
+    function startMove() {
+        if(route && !moveInt){
+            moveInt = setInterval(move,1000/FPS);
+        }
+       
+    }
+    function stopMove() {
+        if(moveInt){
+            clearInterval(moveInt);
+            moveInt = null;
+        }
+    }
+    
+    function move() {
+        var newZombPos;
+        if (route.length>0) {
+            if (isPaused)
+                return;
+            if (getPosition()==route[0]) {
+                route.shift();
+            }
+            if (route[0]) {
+                var nextX = route[0].lat();
+                var nextY = route[0].lng();
+                var zombieX = getPosition().lat();
+                var zombieY = getPosition().lng();
+                var crowLength = Math.sqrt(Math.pow(zombieX-nextX,2)+Math.pow(zombieY-nextY,2));
+                if(zombieMoveDist>crowLength){
+                    newZombPos = route[0];
+                }
+                else{
+                    var pctMove = zombieMoveDist/crowLength;
+                    var newX = zombieX-(pctMove * (zombieX-nextX));
+                    var newY = zombieY-(pctMove * (zombieY-nextY));
+                    newZombPos = new google.maps.LatLng(newX,newY);
+                }
+               setPosition(newZombPos);
+            }
+        }
+        else
+        {
+            clearInterval(moveInt);
+        }
+    }
+    
+    function isInPlayerVisibleRadius() {
+        return (google.maps.geometry.spherical.computeDistanceBetween(mapMarker.getPosition(), playerMarker.getPosition()) <= zombieVisibleRadius);
+    }
+    
+    // Run once
+    google.maps.event.addListener(panoramaMarker, 'click', function() {
+        //hit();
+    });
+    
+    return {
+        getPosition:getPosition,
+        startMove:startMove,
+        stopMove:stopMove,
+        redirect:redirect,
+        isDead:isDead,
+        isInPlayerVisibleRadius:isInPlayerVisibleRadius,
+        spawn:spawn,
+    };
 }
 
 function mainLoop() {
-    
+    var zom;
+    var dist;
+    redirects = 0;
+    var i;
+    for(i in zombies){
+        zom = zombies[i];
+        dist = google.maps.geometry.spherical.computeDistanceBetween(zom.getPosition(),playerMarker.getPosition());
+        if(dist<zombieAwareRadius){
+            redirects++;
+            if(redirects<80){
+                zom.redirect();
+            }
+            zom.startMove();
+        }else if(dist>zombieAsleepRadius){
+            zom.stopMove();
+        }
+    }
 }
 
 var game = {
-    initialize : function() {
+    init : function() {
     
         playerData = JSON.parse(localStorage['data']);
         
@@ -118,6 +315,10 @@ var game = {
                 panorama.setPosition(pos);
                 map.setCenter(pos);
                 
+                if (!positionTracked) {
+                    positionTracked = true;
+                    game.start();
+                }
 
             }, function(error) {
                 if (error.code == error.PERMISSION_DENIED)
@@ -135,6 +336,20 @@ var game = {
         }
         
         setInterval(mainLoop, 1000/FPS);
+    },
+    
+    start : function() {
+        game.createZombies();
+    },
+    
+    createZombies : function() {
+        var newZombie;
+        var i;
+        for(i=1;i<=totalZombies;i++) {
+            newZombie = new Zombie();
+            newZombie.spawn();
+            zombies.push(newZombie);
+        }
     },
     
     toggleView : function() {
@@ -157,6 +372,10 @@ var game = {
             data: playerData
         });
     },
+    
+    isPanoramaView : function () {
+        return !$("#map-canvas").hasClass('bigmap');
+    }
 }
 
-$( game.initialize );
+$( game.init );
