de9b3b117cc9918fab09568cdc8723eb5d58fb5c
Catia Prandi <catia.prandi2@unibo.it>
2015-03-15 17:07:20 +0100

    fixed css problem

--- www/js/ui.js
+++ www/js/ui.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
- $( window ).resize(function() {
- var viewportHeight = $( window ).height();
- var viewportWidth = $( window ).width();
- var controlWidth = (viewportHeight/5);
- $('#map-container').css('width', viewportWidth-controlWidth);
- });
-
- $(window).trigger('resize');
+$( window ).resize(function() {
+var viewportHeight = $( window ).height();
+var viewportWidth = $( window ).width();
+var controlWidth = viewportHeight/5;
+var containerWidth = viewportWidth-controlWidth
+var controlHeight = controlWidth;
+$('#map-container').css('width', containerWidth);
+$('#controls').css('width', controlWidth);
+$('.control-button').css('height', controlHeight);
+});
+$(window).resize();
 });
